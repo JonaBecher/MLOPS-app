db20e37c197fabde5ee8f661276fec8805336bb3
jonabecher <jonajbecher@gmail.com>
2024-01-18 20:56:12 +0100

    updates for mlops@edge

--- src/App.jsx
+++ src/App.jsx
@@ -29,8 +29,8 @@ async function getClientId(baseUrl) {
 
 
 const App = () => {
-  const baseUrl = "http://134.209.232.89:8000/300530342426941322/"
-  const websocketUrl = "ws://134.209.232.89:8000/ws/"
+  const baseUrl = "https://mlops.mbyt.de/300530342426941322/"
+  const websocketUrl = "ws://mlops.mbyt.de:8000/ws/"
   const [loading, setLoading] = useState({ loading: true, progress: 0 });
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
