c657272645d75867be60e80ad1aa79e2ce9cd616
jonabecher <jonajbecher@gmail.com>
2024-01-18 16:05:32 +0100

    updates for mlops@edge

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/yolov7-tfjs/',
-  build: {
-    chunkSizeWarningLimit: 2000,// handle warning on vendor.js bundle size
-  },
+  base: '/yolov7-tfjs/'
 });
